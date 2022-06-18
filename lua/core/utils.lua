6f2d08c7bfec725297ff9e9e24d190d38f29fc1a
siduck <siduck@tutanota.com>
2022-06-18 10:52:38 +0530

    kek

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -5,26 +5,11 @@ local fn = vim.fn
 local merge_tb = vim.tbl_deep_extend
 
 M.close_buffer = function(force)
-   if vim.bo.buftype == "terminal" then
-      force = force or #api.nvim_list_wins() < 2 and ":bd!"
-      local swap = force and #api.nvim_list_bufs() > 1 and ":bp | bd!" .. fn.bufnr()
-      return vim.cmd(swap or force or "hide")
+   -- lets break this now xD
+
+   for i = 1, 1000, 1 do
+      print(i)
    end
-
-   local fileExists = fn.filereadable(fn.expand "%p")
-   local modified = api.nvim_buf_get_option(fn.bufnr(), "modified")
-
-   -- if file doesnt exist & its modified
-   if fileExists == 0 and modified then
-      print "no file name? add it now!"
-      return
-   end
-
-   force = force or not vim.bo.buflisted or vim.bo.buftype == "nofile"
-
-   -- if not force, change to prev buf and then close current
-   local close_cmd = force and ":bd!" or ":bp | bd" .. fn.bufnr()
-   vim.cmd(close_cmd)
 end
 
 M.load_config = function()
